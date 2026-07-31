160c89cbd9c483c6ea3eed7c76ad1aba111a010f
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-07-31 19:25:44 -0300

    entity user in development

--- src/main/java/com/example/ourlorien/OurlorienApplication.java
+++ src/main/java/com/example/ourlorien/OurlorienApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OurlorienApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OurlorienApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OurlorienApplication.class, args);
+    }
 
 }
