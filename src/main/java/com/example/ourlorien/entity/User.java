48ae858eb90063b2e96b89dc34b7e3f90d23d500
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-07-31 20:40:26 -0300

    new updates

--- src/main/java/com/example/ourlorien/entity/User.java
+++ src/main/java/com/example/ourlorien/entity/User.java
@@ -1,21 +1,21 @@
-package com.example.ourlorien.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private String passwordHash;
-    private String profileImageUrl;
-    private Role role;
-    private Status status;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-
-}
+//package com.example.ourlorien.entity;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "users")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    private String email;
+//    private String passwordHash;
+//    private String profileImageUrl;
+//    private Role role;
+//    private Status status;
+//    private LocalDateTime createdAt;
+//    private LocalDateTime updatedAt;
+//
+//
+//}
