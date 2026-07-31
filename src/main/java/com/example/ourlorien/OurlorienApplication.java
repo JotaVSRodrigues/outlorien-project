ff33aed2e5b9e959a187c6f6e28c2f4bfb7b485d
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-07-31 11:21:56 -0300

    first commit

---
+++ src/main/java/com/example/ourlorien/OurlorienApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourlorien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurlorienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurlorienApplication.class, args);
+	}
+
+}
