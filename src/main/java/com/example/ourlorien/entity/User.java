ef058fb68177383019a628e201c9ca3a968f61d6
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-08-01 09:31:50 -0300

    enums Role and Status to define User atributes

--- src/main/java/com/example/ourlorien/entity/User.java
+++ src/main/java/com/example/ourlorien/entity/User.java
@@ -1,21 +1,27 @@
-//package com.example.ourlorien.entity;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//@Table(name = "users")
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String name;
-//    private String email;
-//    private String passwordHash;
-//    private String profileImageUrl;
-//    private Role role;
-//    private Status status;
-//    private LocalDateTime createdAt;
-//    private LocalDateTime updatedAt;
-//
-//
-//}
+package com.example.ourlorien.entity;
+
+import com.example.ourlorien.enums.Role;
+import com.example.ourlorien.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private String profileImageUrl;
+    private Role role;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
